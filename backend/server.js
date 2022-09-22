ee05875a740072f682baff95dd5de65f25fbb6b3
Sandeep-Balli <sandeepballi.tech@gmail.com>
2022-09-23 00:34:08 +0530

    FrontEnd Code Ready ✔

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const workoutRoutes = require('./routes/workout')
 
@@ -8,11 +9,17 @@ const workoutRoutes = require('./routes/workout')
 const app = express();
 
 // middleware
+app.use(cors({origin: true, credentials: true}));
 app.use(express.json())
+// app.use((req, res, next) => {
+//     console.log(req.path, req.method);
+//     next();
+// })
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
+    res.header({"Access-Control-Allow-Origin": "*"});
     next();
-})
+  }) 
 
 // routes
 
